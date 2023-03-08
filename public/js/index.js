4eb180a0920654c74e4a66967621d17fd8bafb61
Martín Javier <martinjavierd@gmail.com>
2023-03-08 19:08:41 +0100

    Armado general

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+const socket = io();
+
+socket.emit("message", "Mensaje desde frontend");
+
+socket.on("message", (data) => {
+  console.log(data);
+});
+
+socket.on("input-changed", (data) => {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = data;
+});
+
+const textInput = document.getElementById("text-input");
+textInput.addEventListener("input", (ev) => {
+  socket.emit("input-changed", ev.target.value);
+});
+
+const sendButton = document.getElementById("send-button");
+sendButton.addEventListener("click", (ev) => {
+  socket.emit("new-message", textInput.value);
+});
--- src/app.js
+++ src/app.js
@@ -1,19 +1,42 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import __dirname from "./utils.js";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let products = [];
 
 const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/../public"));
+app.use("/real-time-products", viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.use(express.static(__dirname + "/../public"));
-
-app.listen(8080, () => {
+const httpServer = app.listen(8080, () => {
   console.log("Server listening on port 8080");
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log("New client connected!");
+
+  socket.on("message", (data) => {
+    socket.emit("input-changed", JSON.stringify(messages));
+  });
+
+  socket.on("input-changed", (data) => {
+    socketServer.emit("input-changed", data);
+  });
+
+  socket.on("new-message", (data) => {
+    messages.push({ socketId: socket.id, mensaje: data });
+    socketServer.emit("input-changed", JSON.stringify(messages));
+  });
 });
