9d3bf048e387f884b41d1461dfc2f4594faee9bd
Martín Javier <martinjavierd@gmail.com>
2023-03-09 20:26:05 +0100

    Enviado un producto

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -6,8 +6,13 @@ const router = Router();
 let products = [];
 
 router.get("/real-time-products", (req, res) => {
+  let prodManager = new ProductManager();
+  let products = prodManager.getProducts();
+  let product = products[0];
+
   res.render("real_time_products", {
-    ...products,
+    ...product,
+    style: "real_time_products",
   });
 });
 
